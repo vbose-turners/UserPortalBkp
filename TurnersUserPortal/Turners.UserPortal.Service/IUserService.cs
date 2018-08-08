b3308028f96d7863cf94c03c4a98ea0cedb09a42
Vijay Bose <Vijay.Bose@turners.co.nz>
2018-08-08 17:40:43 +1200

    Restructured  Service folder

---
+++ TurnersUserPortal/Turners.UserPortal.Service/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Turners.UserPortal.Domain;
+
+namespace Turners.UserPortal.Service
+{
+    public interface IUserService
+    {
+        List<User> GetUsers(string userName, string departmentName);
+    }
+}
